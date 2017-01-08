e5d0e7dbfa04bcd76f7d236dd2a0344160df4ad4
Shubham <2014UGEC078@nitjsr.ac.in>
2017-01-09 02:36:17 +0530

    Changes made to Feed Activity Java Class

--- app/src/main/java/com/seals/shubham/placementportalvolley/FeedActivity.java
+++ app/src/main/java/com/seals/shubham/placementportalvolley/FeedActivity.java
@@ -2,6 +2,13 @@ package com.seals.shubham.placementportalvolley;
 
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -9,16 +16,76 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class FeedActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
-        Toolbar toolbar = (Toolbar)findViewById(R.id.feed_toolbar);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar_feed);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setTitle(R.string.str_feed);
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.feed_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FeedActivity.this,UploadActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        final DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        final NavigationView mNavigationView = (NavigationView)findViewById(R.id.vNavigation);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_log_out:
+                        Intent intent = new Intent(FeedActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+                    case R.id.menu_settings:
+                        break;
+                    case R.id.menu_stats:
+                        Intent intenSt = new Intent(FeedActivity.this, PlacementStats.class);
+                        startActivity(intenSt);
+                        finish();
+                        break;
+                    case R.id.menu_about:
+                        Intent intentAb = new Intent(FeedActivity.this,About_Description.class);
+                        startActivity(intentAb);
+                        finish();
+                        break;
+                }
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawerLayout);
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
     }
 
     @Override
